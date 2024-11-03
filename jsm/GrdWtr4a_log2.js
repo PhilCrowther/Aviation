1d9aae0253af351eef10ae3b6e1e786570b9c9a9
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2024-11-03 11:48:13 -0600

    Add files via upload

--- jsm/GrdWtr4a_log2.js
+++ jsm/GrdWtr4a_log2.js
@@ -133,7 +133,7 @@ constructor(grd_,scene,vertexShader,vertexShaderParams) {
 	this._init1GrMap(this.grd_.Grx[2], grd_,this.scene);
 }
 
-_initGeoMat(grd_,scene,vertexShader,vertexShaderParams,vDepth) {
+_initGeoMat(grd_,scene,vertexShader,vertexShaderParams) {
 // Define Geometries and Materials Referenced in grd_.Geo and grd_.Mat
 	// Grid0 ------------------------------------------------------------------
 	// For Grid0, using geometry = siz*stp since flip over stp at a time
