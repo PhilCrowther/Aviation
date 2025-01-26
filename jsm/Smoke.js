c0e9d624b86805eb984b98f2ddc5821545d095e7
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-01-26 00:37:35 -0600

    Add files via upload

--- jsm/Smoke.js
+++ jsm/Smoke.js
@@ -60,9 +60,49 @@ function initVulkan(vlk_) {
 		vlk_.ObjRef[0].add(vlk_.ObjAdr[0]);
 }
 
+//= INITIALIZE SHIP SMOKE ======================================================
+
+function initShpWak(wak_) {
+	for (let n = 0; n < wak_.ObjNum; n ++) {
+		//- Timer
+		let speed = uniform(.001); // r170 Lower = slower
+		let scaledTime = time.add(125).mul(speed); // r170
+		//- Life
+		let lifeRange = range(0.1,1);
+		let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
+		let life = lifeTime.div(lifeRange);
+		//- Rotation Range
+		let rotateRange = range(.1,.2);
+		let textureNode = texture(wak_.WakTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
+		//- Lateral Offset	
+		let offsetRange = range(new THREE.Vector3(0,3,0), new THREE.Vector3(0,5,0));
+		//- Size Range
+		let scaleRange = range(.01,.02);
+		//
+		let fakeLightEffect = positionLocal.x.oneMinus().max(0.2);
+		// Wake
+		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
+		let	WakMat = new THREE.SpriteNodeMaterial();
+			WakMat.colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
+			WakMat.opacityNode = opacityNode;
+			WakMat.positionNode = offsetRange.mul(lifeTime);
+			WakMat.scaleNode = scaleRange.mul(lifeTime.max(0.3));
+			WakMat.depthWrite = false;
+			WakMat.transparent = true;
+		wak_.ObjAdr[n] = new THREE.Mesh(new THREE.PlaneGeometry(1, 1),WakMat);
+			wak_.ObjAdr[n].scale.setScalar(wak_.ObjSiz[n]);
+			wak_.ObjAdr[n].isInstancedMesh = true;
+			wak_.ObjAdr[n].count = 600; // Increases continuity (was 100)
+			wak_.ObjAdr[n].rotation.x = Math.PI/2; // Set Flat
+			wak_.ObjAdr[n].position.y = -5; // Added
+			wak_.ObjRef[n].add(wak_.ObjAdr[n]);
+	}
+}
+
 //= EXPORTS ====================================================================
 
-export {initVulkan};
+export {initVulkan,initShpWak};
 
 /*= REVISIONS ==================================================================
  * 250125:	Created
