50b08e9a2f1f988e61fc105d209b78851d749314
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-02-01 23:22:10 -0600

    Add files via upload

--- jsm/Smoke.js
+++ jsm/Smoke.js
@@ -89,7 +89,7 @@ function initShpWak(wak_) {
 		let life = lifeTime.div(lifeRange);
 		//- Rotation Range
 		let rotateRange = range(.1,.2);
-		let textureNode = texture(wak_.WakTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let textureNode = texture(wak_.ObjTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
 		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
 		//- Lateral Offset	
 		let offsetRange = range(new Vector3(0,3,0), new Vector3(0,5,0));
@@ -97,60 +97,67 @@ function initShpWak(wak_) {
 		let scaleRange = range(.01,.02);
 		//
 		let fakeLightEffect = positionLocal.x.oneMinus().max(0.2);
-		// Wake
+		//	Color
 		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
-		let	WakMat = new SpriteNodeMaterial();
-			WakMat.colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
-			WakMat.opacityNode = opacityNode;
-			WakMat.positionNode = offsetRange.mul(lifeTime);
-			WakMat.scaleNode = scaleRange.mul(lifeTime.max(0.3));
-			WakMat.depthWrite = false;
-			WakMat.transparent = true;
-		wak_.ObjAdr[n] = new Mesh(new PlaneGeometry(1, 1),WakMat);
-			wak_.ObjAdr[n].scale.setScalar(wak_.ObjSiz[n]);
-			wak_.ObjAdr[n].isInstancedMesh = true;
-			wak_.ObjAdr[n].count = 600; // Increases continuity (was 100)
-			wak_.ObjAdr[n].rotation.x = Math.PI/2; // Set Flat
-			wak_.ObjAdr[n].position.y = -5; // Added
-			wak_.ObjRef[n].add(wak_.ObjAdr[n]);
+		//	Material
+		wak_.ObjMat[n] = new SpriteNodeMaterial();
+		wak_.ObjMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
+		wak_.ObjMat[n].opacityNode = opacityNode;
+		wak_.ObjMat[n].positionNode = offsetRange.mul(lifeTime);
+		wak_.ObjMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
+		wak_.ObjMat[n].depthWrite = false;
+		wak_.ObjMat[n].transparent = true;
+		//	Mesh
+		wak_.ObjAdr[n] = new Mesh(new PlaneGeometry(1, 1),wak_.ObjMat[n]);
+		wak_.ObjAdr[n].scale.setScalar(wak_.ObjSiz[n]);
+		wak_.ObjAdr[n].isInstancedMesh = true;
+		wak_.ObjAdr[n].count = 600; // Increases continuity (was 100)
+		wak_.ObjAdr[n].rotation.x = Math.PI/2; // Set Flat
+		wak_.ObjAdr[n].position.y = -5; // Added
+		//	Link
+		wak_.ObjRef[n].add(wak_.ObjAdr[n]);
 	}
 }
 
 //**************************************|****************************************
 //																				*
-//								 HORIZONTAL SMOKE								*
+//								 AIRPLANE SMOKE									*
 //																				*
 //*******************************************************************************
 
 //= INITIALIZE HORIZONTAL SMOKE =================================================
 
 function initXACSmk(xas_) {
-	let lifeRange = range(0.1,1);
-	let offsetRange = range(new Vector3(0,3,0), new Vector3(0,5,0));
-	let speed = uniform(.2);		// r170
-	let scaledTime = time.add(5).mul(speed); // r170
-	let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
-	let scaleRange = range(.01,.02);
-	let rotateRange = range(.1,4);
-	let life = lifeTime.div(lifeRange);
-	let fakeLightEffect = positionLocal.x.oneMinus().max(0.2);
-	let textureNode = texture(xas_.SmkMap, rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
-	let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);
-	let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
-	let smokeNodeMaterial = new SpriteNodeMaterial();
-		smokeNodeMaterial.colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
-		smokeNodeMaterial.opacityNode = opacityNode;
-		smokeNodeMaterial.positionNode = offsetRange.mul(lifeTime);
-		smokeNodeMaterial.scaleNode = scaleRange.mul(lifeTime.max(0.3));
-		smokeNodeMaterial.depthWrite = false;
-		smokeNodeMaterial.transparent = true;
-	let smokeInstancedSprite = new Mesh(new PlaneGeometry(1, 1), smokeNodeMaterial);
-		smokeInstancedSprite.scale.setScalar(400);
-		smokeInstancedSprite.isInstancedMesh = true;
-		smokeInstancedSprite.count = 100;
-		smokeInstancedSprite.rotation.x = Math.PI/2;
-		smokeInstancedSprite.position.z = 10;
-	xas_.SmkAdr = smokeInstancedSprite;
+	for (let n = 0; n < xas_.ObjNum; n ++) {
+		let lifeRange = range(0.1,1);
+		let offsetRange = range(new Vector3(0,3,0), new Vector3(0,5,0));
+		let speed = uniform(.2);		// r170
+		let scaledTime = time.add(5).mul(speed); // r170
+		let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
+		let scaleRange = range(.01,.02);
+		let rotateRange = range(.1,4);
+		let life = lifeTime.div(lifeRange);
+		let fakeLightEffect = positionLocal.x.oneMinus().max(0.2);
+		let textureNode = texture(xas_.ObjTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);
+		//	Color
+		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
+		//	Material
+		xas_.ObjMat[n] = new SpriteNodeMaterial();
+		xas_.ObjMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
+		xas_.ObjMat[n].opacityNode = opacityNode;
+		xas_.ObjMat[n].positionNode = offsetRange.mul(lifeTime);
+		xas_.ObjMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
+		xas_.ObjMat[n].depthWrite = false;
+		xas_.ObjMat[n].transparent = true;
+		//	Mesh
+		xas_.ObjAdr[n] = new Mesh(new PlaneGeometry(1, 1),xas_.ObjMat[n]);
+		xas_.ObjAdr[n].scale.setScalar(xas_.ObjSiz[n]);
+		xas_.ObjAdr[n].isInstancedMesh = true;
+		xas_.ObjAdr[n].count = 100;
+		xas_.ObjAdr[n].rotation.x = Math.PI/2;
+		xas_.ObjAdr[n].position.z = 10;
+	}
 }
 
 //**************************************|****************************************
