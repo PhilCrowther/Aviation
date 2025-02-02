12c8a5ecd3ef7c31ea6c8fa0e52f3d4ec9e0d761
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-02-02 02:07:31 -0600

    Add files via upload

--- jsm/Smoke.js
+++ jsm/Smoke.js
@@ -35,39 +35,44 @@ import {uniform,range,color,texture,uv,rotateUV,mix,time,positionLocal} from "th
 //= INITIALIZE VOLCANO SMOKE ====================================================
 
 function initVulkan(vlk_) {
-	//- Timer
-	let speed = uniform(.001); // r170
-	let scaledTime = time.add(125).mul(speed); // r170
-	//- Life
-	let lifeRange = range(0.1,1);
-	let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
-	let life = lifeTime.div(lifeRange);
-	//- Rotation Range
-	let rotateRange = range(.1,4);
-	let textureNode = texture(vlk_.VlkTxt[0], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
-	let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
-	//- Lateral Offset	
-	let offsetRange = range(new Vector3(-.5,3,-.5), new Vector3(1,5,1)); // cone shaped
-	//- Size Range
-	let scaleRange = range(.1,.2);
-	//
-	let fakeLightEffect = positionLocal.y.oneMinus().max(0.2);
-	// Wake
-	let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
-	let VlkMat = new SpriteNodeMaterial();
-		VlkMat.colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
-		VlkMat.opacityNode = opacityNode;
-		VlkMat.positionNode = offsetRange.mul(lifeTime);
-		VlkMat.scaleNode = scaleRange.mul(lifeTime.max(0.3));
-		VlkMat.depthWrite = false;
-		VlkMat.transparent = true;
-	vlk_.ObjAdr[0] = new Mesh(new PlaneGeometry(1, 1),VlkMat);
-		vlk_.ObjAdr[0].scale.setScalar(vlk_.ObjSiz[0]);
-		vlk_.ObjAdr[0].isInstancedMesh = true;
-		vlk_.ObjAdr[0].count = 600; // Increases continuity (was 100)
-		vlk_.ObjAdr[0].position.copy(vlk_.MapPos[0]);
-		vlk_.ObjAdr[0].renderOrder = vlk_.RndOrd[0]; // This allows the transparent smoke to work with transparent island
-		vlk_.ObjRef[0].add(vlk_.ObjAdr[0]);
+	for (let n = 0; n < vlk_.ObjNum; n ++) {
+		//- Timer
+		let speed = uniform(.001); // r170
+		let scaledTime = time.add(125).mul(speed); // r170
+		//- Life
+		let lifeRange = range(0.1,1);
+		let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
+		let life = lifeTime.div(lifeRange);
+		//- Rotation Range
+		let rotateRange = range(.1,4);
+		let textureNode = texture(vlk_.ObjTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
+		//- Lateral Offset	
+		let offsetRange = range(new Vector3(-.5,3,-.5), new Vector3(1,5,1)); // cone shaped
+		//- Size Range
+		let scaleRange = range(.1,.2);
+		//
+		let fakeLightEffect = positionLocal.y.oneMinus().max(0.2);
+		// Wake
+		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
+		//	Material
+		vlk_.ObjMat[n] = new SpriteNodeMaterial();
+		vlk_.ObjMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
+		vlk_.ObjMat[n].opacityNode = opacityNode;
+		vlk_.ObjMat[n].positionNode = offsetRange.mul(lifeTime);
+		vlk_.ObjMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
+		vlk_.ObjMat[n].depthWrite = false;
+		vlk_.ObjMat[n].transparent = true;
+		//	Mesh
+		vlk_.ObjAdr[n] = new Mesh(new PlaneGeometry(1, 1),vlk_.ObjMat[n]);
+		vlk_.ObjAdr[n].scale.setScalar(vlk_.ObjSiz[n]);
+		vlk_.ObjAdr[n].isInstancedMesh = true;
+		vlk_.ObjAdr[n].count = 600; // Increases continuity (was 100)
+		vlk_.ObjAdr[n].position.copy(vlk_.MapPos[n]);
+		vlk_.ObjAdr[n].renderOrder = vlk_.RndOrd[n]; // This allows the transparent smoke to work with transparent island
+		//lINK
+		vlk_.ObjRef[n].add(vlk_.ObjAdr[n]);
+	}
 }
 
 //**************************************|****************************************
