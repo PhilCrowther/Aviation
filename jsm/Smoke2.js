99129134367cdb7bb605886e501f6ca20c737495
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-03-10 10:27:05 -0500

    Add files via upload

--- jsm/Smoke2.js
+++ jsm/Smoke2.js
@@ -46,7 +46,7 @@ function initGrdSmk(grs_) {
 		let life = lifeTime.div(lifeRange);
 		//- Rotation Range
 		let rotateRange = range(.1,4);
-		let textureNode = texture(grs_.ObjTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let textureNode = texture(grs_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
 		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
 		//- Lateral Offset	
 		let offsetRange = range(new Vector3(-.5,3,-.5), new Vector3(1,5,1)); // cone shaped
@@ -66,13 +66,10 @@ function initGrdSmk(grs_) {
 		grs_.SmkMat[n].transparent = true;
 		//-	Mesh
 		grs_.SmkMsh[n] = new Mesh(new PlaneGeometry(1, 1),grs_.SmkMat[n]);
-		grs_.SmkMsh[n].scale.setScalar(grs_.ObjSiz[n]);
+		grs_.SmkMsh[n].scale.setScalar(grs_.ObjSiz);
 		grs_.SmkMsh[n].isInstancedMesh = true;
 		grs_.SmkMsh[n].count = 600; // Increases continuity (was 100)
-		grs_.SmkMsh[n].position.copy(grs_.MapPos[n]);
-		grs_.SmkMsh[n].renderOrder = grs_.RndOrd[n]; // This allows the transparent smoke to work with transparent island
-		//-	Link
-		grs_.ObjRef[n].add(grs_.SmkMsh[n]);
+		grs_.SmkMsh[n].renderOrder = 1; // This allows the transparent smoke to work with transparent island
 	}
 }
 
@@ -103,9 +100,7 @@ function initGrdFyr(grf_) {
 		grf_.SmkMsh[n] = new Mesh(new PlaneGeometry(1,1),grf_.SmkMat[n]);
 		grf_.SmkMsh[n].scale.setScalar(grf_.ObjSiz);
 		grf_.SmkMsh[n].count = 2000;
-		//
-		grf_.SmkMsh[n].position.copy(grf_.MapPos[n]);
-		grf_.ObjRef[n].add(grf_.SmkMsh[n]);
+		grf_.SmkMsh[n].renderOrder = 1;
 		//- Fire
 		grf_.FyrMat[n] = new SpriteNodeMaterial();
 		grf_.FyrMat[n].colorNode = mix(color(0xb72f17),color(0xb72f17),life);
@@ -120,10 +115,6 @@ function initGrdFyr(grf_) {
 		grf_.FyrMsh[n].scale.setScalar(grf_.ObjSiz);
 		grf_.FyrMsh[n].count = 1000;
 		grf_.FyrMsh[n].renderOrder = 1;
-		//
-		grf_.FyrMsh[n].position.copy(grf_.MapPos[n]);
-		grf_.ObjRef[n].add(grf_.FyrMsh[n]);
-//		grf_.FyrMsh[n].position.y = -100;
 	}
 }
 
@@ -146,25 +137,25 @@ function initAirSmk(xas_) {
 		let rotateRange = range(.1,4);
 		let life = lifeTime.div(lifeRange);
 		let fakeLightEffect = positionLocal.x.oneMinus().max(0.2);
-		let textureNode = texture(xas_.ObjTxt[n], rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let textureNode = texture(xas_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
 		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);
 		//	Color
 		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
 		//	Material
-		xas_.ObjMat[n] = new SpriteNodeMaterial();
-		xas_.ObjMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
-		xas_.ObjMat[n].opacityNode = opacityNode;
-		xas_.ObjMat[n].positionNode = offsetRange.mul(lifeTime);
-		xas_.ObjMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
-		xas_.ObjMat[n].depthWrite = false;
-		xas_.ObjMat[n].transparent = true;
+		xas_.SmkMat[n] = new SpriteNodeMaterial();
+		xas_.SmkMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
+		xas_.SmkMat[n].opacityNode = opacityNode;
+		xas_.SmkMat[n].positionNode = offsetRange.mul(lifeTime);
+		xas_.SmkMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
+		xas_.SmkMat[n].depthWrite = false;
+		xas_.SmkMat[n].transparent = true;
 		//	Mesh
-		xas_.ObjAdr[n] = new Mesh(new PlaneGeometry(1, 1),xas_.ObjMat[n]);
-		xas_.ObjAdr[n].scale.setScalar(xas_.ObjSiz[n]);
-		xas_.ObjAdr[n].isInstancedMesh = true;
-		xas_.ObjAdr[n].count = 100;
-		xas_.ObjAdr[n].rotation.x = Math.PI/2;
-		xas_.ObjAdr[n].position.z = 10;
+		xas_.SmkMsh[n] = new Mesh(new PlaneGeometry(1, 1),xas_.SmkMat[n]);
+		xas_.SmkMsh[n].scale.setScalar(xas_.ObjSiz);
+		xas_.SmkMsh[n].isInstancedMesh = true;
+		xas_.SmkMsh[n].count = 100;
+		xas_.SmkMsh[n].rotation.x = Math.PI/2;
+		xas_.SmkMsh[n].position.z = 10;
 	}
 }
 
@@ -172,12 +163,12 @@ function initAirSmk(xas_) {
 
 function initAirFyr(xaf_) {
 	for (let n = 0; n < xaf_.ObjNum; n ++) {
-		let lifeRange = range(.1,1);
-		let speed = uniform(.2);
+		let lifeRange = range(0.1,1);
+		let speed = uniform(0.2);
 		let scaledTime = time.add(5).mul(speed);
 		let lifeTime = scaledTime.mul(lifeRange).mod(1);
 		let scaleRange = range(.3,2);
-		let rotateRange = range(.1,4);
+		let rotateRange = range(0.1,4);
 		let life = lifeTime.div(lifeRange);
 		let fakeLightEffect = positionLocal.y.oneMinus().max(0.2);
 		let textureNode = texture(xaf_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange)));
