c3c0eb5a447346de04d81a1202c52810c91b316c
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-04-04 00:28:10 -0700

    Add files via upload

--- jsm/GrdWtr4b.js
+++ jsm/GrdWtr4b.js
@@ -83,59 +83,59 @@ constructor(grd_,scene) {
 	this.grd_ = grd_;
 	this.scene = scene;
 
-//- Grid 0 ---------------------------------------------------------------------
+//- Grid 0 ---------------------//----------------------------------------------
 	this.grd_.Grx[0] = {
-		Typ:	0,					// Type of Grid - Inner or Outer
-		RCs:	grd_.RCs/grd_.Stp,	// Since each square is 4x4, only need 4x4 
-		Siz:	grd_.Siz*grd_.Stp,	// Size of square = Sizx4
-		Stp:	1,					// Steps (only 1 since use 4x4 squares)
-		RCi:	0,					// Rows and Columns Index (computed)
-		MZV:	[0],				// Ground Z Value
-		MXV:	[0],				// Ground X Value
-		Nor:	0,					// Max North Square (updated)
-		Est:	0,					// Max East Square (updated)
-		Num:	0,					// Size of array (computed)
-		Ptr:	[0],				// Ground Address
-		Shd:	1,					// Shadow enabled
-		RCF:	0,					// N/A
-		NSA:	0,					// N/A
-		EWA:	0,					// N/A
+		Typ:	0,				// Type of Grid - Inner or Outer
+		RCs:	grd_.RCs/grd_.Stp, // Since each square is 4x4, only need 4x4 
+		Siz:	grd_.Siz*grd_.Stp, // Size of square = Sizx4
+		Stp:	1,				// Steps (only 1 since use 4x4 squares)
+		RCi:	0,				// Rows and Columns Index (computed)
+		MZV:	[0],			// Ground Z Value
+		MXV:	[0],			// Ground X Value
+		Nor:	0,				// Max North Square (updated)
+		Est:	0,				// Max East Square (updated)
+		Num:	0,				// Size of array (computed)
+		Ptr:	[0],			// Ground Address
+		Shd:	1,				// Shadow enabled
+		RCF:	0,				// N/A
+		NSA:	0,				// N/A
+		EWA:	0,				// N/A
 	}
-//- Grid 1 ---------------------------------------------------------------------
+//- Grid 1 ---------------------//----------------------------------------------
 	this.grd_.Grx[1] = {
-		Typ:	1,					// Type of Grid - Inner or Outer
-		RCs:	grd_.RCs,			// Rows and Columns - use odd number
-		Siz:	grd_.Grx[0].Siz,	// Size of square = same as Grid0
-		Stp:	grd_.Stp,			// Steps
-		RCi:	0,					// Rows and Columns Index (computed)
-		MZV:	[0],				// Ground Z Value
-		MXV:	[0],				// Ground X Value
-		Nor:	0,					// Max North Square (updated)
-		Est:	0,					// Max East Square (updated)
-		Num:	0,					// Size of array (computed)
-		Ptr:	[0],				// Ground Address
-		Shd:	0,					// Shadow enabled ### off for outergrids
-		RCF:	grd_.Grx[0].RCs/grd_.Grx[0].Stp,	// Cut-Out Area (4x4)
-		NSA:	0,					// Shared North/South Adjustment (updated)
-		EWA:	0,					// Shared East/West Adjustment (updated)
+		Typ:	1,				// Type of Grid - Inner or Outer
+		RCs:	grd_.RCs,		// Rows and Columns - use odd number
+		Siz:	grd_.Grx[0].Siz, // Size of square = same as Grid0
+		Stp:	grd_.Stp,		// Steps
+		RCi:	0,				// Rows and Columns Index (computed)
+		MZV:	[0],			// Ground Z Value
+		MXV:	[0],			// Ground X Value
+		Nor:	0,				// Max North Square (updated)
+		Est:	0,				// Max East Square (updated)
+		Num:	0,				// Size of array (computed)
+		Ptr:	[0],			// Ground Address
+		Shd:	0,				// Shadow enabled ### off for outergrids
+		RCF:	grd_.Grx[0].RCs/grd_.Grx[0].Stp, // Cut-Out Area (4x4)
+		NSA:	0,				// Shared North/South Adjustment (updated)
+		EWA:	0,				// Shared East/West Adjustment (updated)
 	}
-//- Grid 2 ---------------------------------------------------------------------
+//- Grid 2 ---------------------//----------------------------------------------
 	this.grd_.Grx[2] = {
-		Typ:	2,					// Type of Grid - Inner or Outer
-		RCs:	grd_.RCs,			// Rows and Columns - use odd number (for now = divisible by 3)
-		Siz:	grd_.Grx[1].Siz*grd_.Grx[1].Stp,	// Size of square
-		Stp:	1,					// Squares to flip
-		RCi:	0,					// Rows and Columns Index (computed)
-		MZV:	[0],				// Ground Z Value
-		MXV:	[0],				// Ground X Value
-		Nor:	0,					// Max North Square (updated)
-		Est:	0,					// Max East Square (updated)
-		Num:	0,					// Size of array (computed)
-		Ptr:	[0],				// Ground Address
-		Shd:	0,					// Shadow enabled ### off for outergrids
-		RCF:	grd_.Grx[1].RCs/grd_.Grx[1].Stp,	// Cut-Out Area (16x16)
-		NSA:	0,					// Shared North/South Adjustment (updated)
-		EWA:	0,					// Shared East/West Adjustment (updated)
+		Typ:	2,				// Type of Grid - Inner or Outer
+		RCs:	grd_.RCs,		// Rows and Columns - use odd number (for now = divisible by 3)
+		Siz:	grd_.Grx[1].Siz*grd_.Grx[1].Stp, // Size of square
+		Stp:	1,				// Squares to flip
+		RCi:	0,				// Rows and Columns Index (computed)
+		MZV:	[0],			// Ground Z Value
+		MXV:	[0],			// Ground X Value
+		Nor:	0,				// Max North Square (updated)
+		Est:	0,				// Max East Square (updated)
+		Num:	0,				// Size of array (computed)
+		Ptr:	[0],			// Ground Address
+		Shd:	0,				// Shadow enabled ### off for outergrids
+		RCF:	grd_.Grx[1].RCs/grd_.Grx[1].Stp, // Cut-Out Area (16x16)
+		NSA:	0,				// Shared North/South Adjustment (updated)
+		EWA:	0,				// Shared East/West Adjustment (updated)
 	}
 	this.grd_.Nrm.repeat.set(this.grd_.Stp,this.grd_.Stp);
 	this.grd_.Dsp.repeat.set(this.grd_.Stp,this.grd_.Stp);
@@ -147,88 +147,89 @@ constructor(grd_,scene) {
 
 _initGeoMat(grd_,scene) {
 // Define Geometries and Materials Referenced in grd_.Geo and grd_.Mat
-	// Grid0 -------------------------------------------------------------------
+// Grid0 and Grid1 are the same size since must flip over same size of Grid0 as matches Grid1
+	// Grid0 -------------------//----------------------------------------------
 	// For Grid0, using geometry = siz*stp since flip over stp at a time
-	// Color texture is full-sized, normal and displacement maps repeat
+	// Color texture is full-sized, normal and displacement maps repeat (lines 140-141)
 	let n = 0;
 	let idx = 0;
-	let normalMapScale = grd_.NMS;
 	// Create 4 Textures
 	for (let z = 0; z < 4; z++) {
 		for (let x = 0; x < 4; x++) {
 			grd_.Mt0[idx] = new MeshStandardNodeMaterial({
 				colorNode: color(grd_.Col),
-				map: grd_.Df0[idx],		// not texture
-				metalness: 0.5,			// 1 for max reflection
-				roughness: 0.5,			// 0 for max reflection
+				map: grd_.Df0[idx], // not texture
+				metalness: 0.5, // 1 for max reflection
+				roughness: 0.5, // 0 for max reflection
 				roughnessMap: grd_.Rf0[idx], // not texture
-				normalNode: normalMap(texture(grd_.Nrm),normalMapScale),
+				normalNode: normalMap(texture(grd_.Nrm),grd_.NMS),
 				positionNode: positionLocal.add(texture(grd_.Dsp)), // must be texture
 				envMap: scene.background,			
-				envMapIntensity: 0.5,		// max reflection suggested = 5
+				envMapIntensity: 0.5, // max reflection suggested = 5
 			});
 			idx++
 		}
 	}
-	// Single 4x4 Geometry works for all
-	let sz0 = grd_.Siz*grd_.Stp;
-	let sg0 = grd_.Seg*grd_.Stp;
-	grd_.Geo[n] = new PlaneGeometry(sz0,sz0,sg0,sg0);
-	grd_.Geo[n].rotateX(-Math.PI*0.5);
-	//- Grid1 ------------------------------------------------------------------
+	//- Grid1 ------------------//----------------------------------------------
 	// For Grid1, using geometry = siz*stp
-	// Color texture is full-sized, normal and displacement maps repeat
-	n = 1;
+	// Grid1 has no displacement
+	// Color texture is full-sized, normal map repeats (line 140)
 	idx = 0;
 	for (let z = 0; z < 4; z++) {
 		for (let x = 0; x < 4; x++) {
 			grd_.Mt1[idx] = new MeshStandardNodeMaterial({	// Normal Map Only, 
 				colorNode: color(grd_.Col),
 				map: grd_.Df0[idx],	// not texture
-				metalness: 0.5,		// 1 for max reflection
-				roughness: 0.5,		// 0 for max reflection
+				metalness: 0.5, // 1 for max reflection
+				roughness: 0.5, // 0 for max reflection
 				roughnessMap: grd_.Rf0[idx], // not texture
-				normalNode: normalMap(texture(grd_.Nrm),normalMapScale),
+				normalNode: normalMap(texture(grd_.Nrm),grd_.NMS),
 				envMap: scene.background,
-				envMapIntensity: 0.5,	// max reflection suggested = 5	
+				envMapIntensity: 0.5, // max reflection suggested = 5	
 			});
 			idx++
 		}
 	}
-	// Single Geometry works for all
-	grd_.Geo[n] = new PlaneGeometry(sz0,sz0);
-	grd_.Geo[n].rotateX(-Math.PI*0.5);
-	//- Grid2 ------------------------------------------------------------------
-	n = 2;
-	grd_.Mat[n] = new MeshStandardNodeMaterial({
+	//- Grid2 ------------------//----------------------------------------------
+	// Grid2 has generic normal map
+	grd_.Mat[2] = new MeshStandardNodeMaterial({
 		colorNode: color(grd_.Col),
 		map: grd_.Dif,			// Full-Sized Texture
 		metalness: 0.5,			// 1 for max reflection
 		roughness: 0.5,			// 0 for max reflection
 		roughnessMap: grd_.Ruf,	// not texture
-		normalNode: normalMap(texture(grd_.Gr2),normalMapScale),
+		normalNode: normalMap(texture(grd_.Gr2),grd_.NMS),
 		envMap: scene.background,
-		envMapIntensity: 0.5,		// max reflection suggested = 5
+		envMapIntensity: 0.5,	// max reflection suggested = 5
 	});
-	// Single Geometry works for all
+	// Geometries --------------------------------------------------------------
+	// Grid0
+	let sz0 = grd_.Siz*grd_.Stp;
+	let sg0 = grd_.Seg*grd_.Stp;
+	grd_.Geo[0] = new PlaneGeometry(sz0,sz0,sg0,sg0);
+	grd_.Geo[0].rotateX(-Math.PI/2);
+	// Grid1 (same size)
+	grd_.Geo[1] = new PlaneGeometry(sz0,sz0);
+	grd_.Geo[1].rotateX(-Math.PI/2);	
+	// Grid2
 	let sz1 = sz0*grd_.Stp;
-	grd_.Geo[n] = new PlaneGeometry(sz1,sz1);
-	grd_.Geo[n].rotateX(-Math.PI*0.5);
+	grd_.Geo[2] = new PlaneGeometry(sz1,sz1);
+	grd_.Geo[2].rotateX(-Math.PI/2);
 }
 
-//- Init Moving Map (Ocean) ----------------------------------------------------
+//- Init Moving Map (Ocean) ----//----------------------------------------------
 
 _init1GrMap(grx_, grd_, scene) {
 	// Load Variables
-	grx_.RCi = grx_.RCs-1;					// Max Index Value
-	grx_.MZV[grx_.RCi] = 0;					// Z-Values
-	grx_.MXV[grx_.RCi] = 0;					// X-Values
-	grx_.Nor = grx_.RCi;					// Max North Square (updated)
-	grx_.Est = grx_.RCi;					// Max East Square (updated)
-	grx_.Num = grx_.RCs**2;					// Size of array
-	grx_.Ptr[grx_.Num-1] = 0;				// Mesh Pointers
+	grx_.RCi = grx_.RCs-1;		// Max Index Value
+	grx_.MZV[grx_.RCi] = 0;		// Z-Values
+	grx_.MXV[grx_.RCi] = 0;		// X-Values
+	grx_.Nor = grx_.RCi;		// Max North Square (updated)
+	grx_.Est = grx_.RCi;		// Max East Square (updated)
+	grx_.Num = grx_.RCs**2;		// Size of array
+	grx_.Ptr[grx_.Num-1] = 0;	// Mesh Pointers
 	if (grx_.Typ > 0) {
-		grx_.NSA = (grx_.RCs-grx_.RCF)/2;	// (27-3=6)
+		grx_.NSA = (grx_.RCs-grx_.RCF)/2; // (27-3=6)
 		grx_.EWA = grx_.NSA;
 	}
 	// Compute Starting Z and X Values
@@ -240,17 +241,17 @@ _init1GrMap(grx_, grd_, scene) {
 	}
 	// Load Geometry and Material
 	let geometry = grd_.Geo[grx_.Typ];
-	let material = grd_.Mat[grx_.Typ];			// Grid 2 only
+	let material = grd_.Mat[grx_.Typ]; // Grid 2 only
 	// Set Starting Position of Squares
 	let n = 0;
 	let idx;
-	for (let z = 0; z < grx_.RCs; z++) {		// Row
-		for (let x = 0; x < grx_.RCs; x++) {	// Column
-			if (grx_.Typ == 0) {				// Grid 0 Load one of 4x4 materials into 4x4 places
+	for (let z = 0; z < grx_.RCs; z++) { // Row
+		for (let x = 0; x < grx_.RCs; x++) { // Column
+			if (grx_.Typ == 0) {	// Grid 0 Load one of 4x4 materials into 4x4 places
 				idx = ((x%4)+2)%4 + ((z%4)+2)%4*4;	// Center X and Z
 				material = grd_.Mt0[idx];
 			}
-			if (grx_.Typ == 1) {				// Grid 1 Load one of 4x4 materials into 16x16 places
+			if (grx_.Typ == 1) { // Grid 1 Load one of 4x4 materials into 16x16 places
 				idx = (x%4) + (z%4)*4;
 				material = grd_.Mt1[idx];
 			}
@@ -278,7 +279,7 @@ update() {
 	this._move1GrMap(this.grd_.Grx[2],this.grd_);
 }
 
-//- Move Moving Map ------------------------------------------------------------
+//- Move Moving Map ------------//----------------------------------------------
 
 _move1GrMap(grx_, grd_) {
 	let grd1_ = grd_.Grx[1];
@@ -292,7 +293,7 @@ _move1GrMap(grx_, grd_) {
 		grx_.MXV[i] = grx_.MXV[i] - grd_.MSP.x;	// Columns
 	}
 	// Test North/South
-	if (grd_.MSP.z < 0) {					// If Moving South
+	if (grd_.MSP.z < 0) {		// If Moving South
 		j = grx_.Nor;
 		if (grx_.MZV[j] >= max) {
 			v = min+(grx_.MZV[j]-max);
@@ -312,7 +313,7 @@ _move1GrMap(grx_, grd_) {
 			if (grx_.Typ == 2) grd2_.NSA = grd2_.NSA + grx_.Stp;
 		}
 	}
-	if (grd_.MSP.z > 0) {					// If Moving North
+	if (grd_.MSP.z > 0) {		// If Moving North
 		j = grx_.Nor + 1;
 		if (j > grx_.RCi) j = 0;
 		if (grx_.MZV[j] <= min) {
@@ -334,7 +335,7 @@ _move1GrMap(grx_, grd_) {
 		}
 	}
 	// Test East/West
-	if (grd_.MSP.x < 0) {					// If Moving West
+	if (grd_.MSP.x < 0) {		// If Moving West
 		j = grx_.Est;
 		if (grx_.MXV[j] >= max) {
 			v = min+(grx_.MXV[j]-max);
@@ -354,7 +355,7 @@ _move1GrMap(grx_, grd_) {
 			if (grx_.Typ == 2) grd2_.EWA = grd2_.EWA + grx_.Stp;
 		}
 	}
-	if (grd_.MSP.x > 0) {						// If Moving East
+	if (grd_.MSP.x > 0) {		// If Moving East
 		j = grx_.Est + 1;
 		if (j > grx_.RCi) j = 0;	
 		if (grx_.MXV[j] <= min) {
@@ -377,8 +378,8 @@ _move1GrMap(grx_, grd_) {
 	}
 	// Set Position
 	let n = 0;
-	for (let r = 0; r < grx_.RCs; r++) {	// Row
-		for (let c = 0; c < grx_.RCs; c++) {	// Col
+	for (let r = 0; r < grx_.RCs; r++) { // Row
+		for (let c = 0; c < grx_.RCs; c++) { // Col
 			grx_.Ptr[n].position.set(grx_.MXV[c],-grd_.MSP.y,-grx_.MZV[r]);
 			if (grx_.Typ > 0) grx_.Ptr[n].position.y = -grd_.MSP.y-grd_.WMx;
 			grx_.Ptr[n].visible = true;	// Default for Outer Grid
@@ -387,9 +388,9 @@ _move1GrMap(grx_, grd_) {
 	}
 	// Outer Grids Only - Make Cut-Out Area Invisible
 	if (grx_.Typ > 0) {
-		let r = grx_.Nor + 1 + grx_.NSA;		// Get Lower index
+		let r = grx_.Nor + 1 + grx_.NSA; // Get Lower index
 		if (r > grx_.RCi) r = r - grx_.RCs;
-		let c = grx_.Est + 1 + grx_.EWA;		// Get Left Index
+		let c = grx_.Est + 1 + grx_.EWA; // Get Left Index
 		if (c > grx_.RCi) c = c - grx_.RCs;
 		for (let i = 0; i < grx_.RCF; i++) {
 			n = r * grx_.RCs + c;
