30368f302a77cfd32ccccac0045d88924b9e8c1f
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-06-02 00:19:04 -0500

    Add files via upload

--- jsm/Objects.js
+++ jsm/Objects.js
@@ -1,5 +1,5 @@
 /*
- * Objects.js (vers 25.05.29)
+ * Objects.js (vers 25.06.22)
  * Copyright 2022-2025, Phil Crowther
  * Licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 */
@@ -34,9 +34,6 @@ import {
 	AnimationClip,
 	AnimationMixer,
 	Vector3,
-	// makMsh
-	BoxGeometry,
-	MeshBasicNodeMaterial,
 } from 'three';
 
 import {color,texture} from "three/tsl";
@@ -207,158 +204,6 @@ function moveAnmFlg(flg_,tim_) {
 		flg_.ObjAdr[n].rotation.y = flg_.ObjRot[n].y+FlgAdj;
 	}
 }
-
-
-/*******************************************************************************
-*
-*	AIRPLANES
-*
-*******************************************************************************/
-
-//=	LOAD AIRPLANES =============//==============================================
-
-function loadXACVeh(gltfLoader,xac_) {
-	for (let n = 0; n < xac_.ObjNum; n ++) {
-		gltfLoader.load(xac_.ObjSrc[n], function (gltf) {
-			xac_.ObjAdr[n] = gltf.scene;
-			// Convert from feet to meters
-			xac_.ObjAdr[n].scale.setScalar(xac_.ObjSiz[n]);
-			// Propeller
-			let clip = AnimationClip.findByName(gltf.animations, "propellerAction");
-			xac_.MixSpn[n] = new AnimationMixer(xac_.ObjAdr[n]);
-			let actun = xac_.MixSpn[n].clipAction(clip);
-			actun.play();
-			if (xac_.MixSpn[n]) xac_.MixSpn[n].setTime(anm_.spnprp/anm_.anmfps);
-			// Bank
-			clip = AnimationClip.findByName(gltf.animations, "AC_BankAction");
-			xac_.MixBnk[n] = new AnimationMixer(xac_.ObjAdr[n]);
-			actun = xac_.MixBnk[n].clipAction(clip);
-			actun.play();
-			if (xac_.MixBnk[n]) xac_.MixBnk[n].setTime(xac_.AnmBnk[n]/anm_.anmfps);
-			// Pitch
-			clip = AnimationClip.findByName(gltf.animations, "AC_PtchAction");
-			xac_.MixPit[n] = new AnimationMixer(xac_.ObjAdr[n]);
-			actun = xac_.MixPit[n].clipAction(clip);
-			actun.play();
-			if (xac_.MixPit[n]) xac_.MixPit[n].setTime(xac_.AnmPit[n]/anm_.anmfps);
-			// Rotation
-			xac_.ObjAdr[n].rotation.order = "YXZ"; // Heading, Pitch, Bank
-			xac_.ObjAdr[n].rotation.y = xac_.ObjRot[n].y*DegRad;
-		});
-	}
-
-}
-
-//=	INIT AIRPLANES =============//==============================================
-
-function initXACVeh(xac_,air_,scene) {
-	for (let n = 0; n < xac_.ObjNum; n ++) {
-		// Compute Relative Position
-		// (cause Objects to elevate above water as we climb to prevent flicker)
-		let X = xac_.MapPos[n].x-air_.MapPos.x;
-		let Y = xac_.MapPos[n].y-gen_.AltDif;
-		let Z = air_.MapPos.z-xac_.MapPos[n].z;
-		xac_.ObjAdr[n].position.set(X,Y,Z);
-		scene.add(xac_.ObjAdr[n]);
-	};
-};
-
-//=	MOVE AIRPLANES ===============//============================================
-
-
-/*******************************************************************************
-*
-*	SHIPS
-*
-*******************************************************************************/
-
-//=	LOAD SHIPS =================//==============================================
-
-function loadXSHVeh(gltfLoader,xsh_) {
-	for (let n = 0; n < xsh_.ObjNum; n ++) {
-		gltfLoader.load(xsh_.ObjSrc[n], function (gltf) {
-			gltf.scene.traverse(function (child) {
-				if (child.isMesh) {
-					child.castShadow = true;
-					child.receiveShadow = true;
-				}
-			});
-			xsh_.ObjAdr[n] = gltf.scene;
-			xsh_.ObjAdr[n].scale.setScalar(xsh_.ObjSiz[n]); // Scale
-			// Animated Radar
-			if (n == 0) {		// if CVE
-				// Radar
-				let clip = AnimationClip.findByName(gltf.animations, "RadarAction");
-				xsh_.MixRdr[n] = new AnimationMixer(xsh_.ObjAdr[n]);
-				let actun = xsh_.MixRdr[n].clipAction(clip);
-				actun.play();
-				if (xsh_.MixRdr[n]) xsh_.MixRdr[n].setTime(xsh_.AnmRdr[n]/anm_.anmfps);
-			}
-			xsh_.ObjAdr[n].position.set(0,0,0); // position within group is always 0,0,0
-		});
-	}
-}
-
-//=	INIT SHIPS =================//==============================================
-
-function initXSHVeh(xsh_,air_,scene) {
-// Always use group
-	let X, Y, Z;
-	for (let n = 0; n < xsh_.ObjNum; n ++) {
-		xsh_.ObjGrp[n].rotation.order = "YXZ";
-		// Compute Relative Position
-		// (cause Objects to elevate above water as we climb to prevent flicker)
-		X = xsh_.MapPos[n].x-air_.MapPos.x;
-		Y = xsh_.MapPos[n].y-gen_.AltDif;
-		Z = air_.MapPos.z-xsh_.MapPos[n].z;
-		xsh_.ObjGrp[n].position.set(X,Y,Z);
-		xsh_.ObjGrp[n].add(xsh_.ObjAdr[n]);
-		scene.add(xsh_.ObjGrp[n]);		// Uses position of CVE to compute relative position
-	}
-}
-
-//=	MOVE SHIPS =================//==============================================
-
-function moveXSHVeh(xsh_,air_) {
-	let X,Y,Z;
-	for (let n = 0; n < xsh_.ObjNum; n ++) {
-		// Change in Heading
-		let XSHSpd = 0;		// for now
-		let XSHPit = 0;
-		XSHSpd = XSHSpd * tim_.DLTime;
-		xsh_.ObjRot[n].y = xsh_.ObjRot[n].y + XSHSpd;	
-		// Rock the boat
-		if (n == 0) {
-			xsh_.ShpPit[n] = Mod360(xsh_.ShpPit[n] + 0.5);
-			let PitAdj = 1.5*DegRad*Math.sin(xsh_.ShpPit[n]*DegRad);
-			xsh_.ObjRot[n].x = PitAdj;
-			xsh_.ObjGrp[n].rotation.copy(xsh_.ObjRot[n]);
-		}
-		// Speed (Only Horizontal)
-		let SpdMPF = xsh_.SpdMPS[n] * tim_.DLTime; // Speed (u/t)
-		xsh_.MapSpd[n].z = -SpdMPF * Math.cos(xsh_.ObjRot[n].y*DegRad);
-		xsh_.MapSpd[n].x = -SpdMPF * Math.sin(xsh_.ObjRot[n].y*DegRad);
-		// Recompute Map Postion
-		xsh_.MapPos[n].x = xsh_.MapPos[n].x + xsh_.MapSpd[n].x;
-		xsh_.MapPos[n].z = xsh_.MapPos[n].z - xsh_.MapSpd[n].z;
-		// Compute New Relative Position
-		X = xsh_.MapPos[n].x-air_.MapPos.x;
-		Y = xsh_.MapPos[n].y-gen_.AltDif;
-		Z = air_.MapPos.z-xsh_.MapPos[n].z;
-		xsh_.ObjGrp[n].position.set(X,Y,Z);
-		// Compute Distance (for Viz Tests)
-		X = xsh_.ObjGrp[n].position.x;
-		Z = xsh_.ObjGrp[n].position.z;
-		xsh_.ObjDst[n] = Math.sqrt(X*X+Z*Z); // Compute distance
-		// Attached Objects
-		if (n == 0) {
-			// Radar
-			xsh_.AnmRdr[n] = xsh_.AnmRdr[n] - 0.1;
-			if (xsh_.AnmRdr[n] < 0) xsh_.AnmRdr[n] = 359;
-			if (xsh_.MixRdr[n]) xsh_.MixRdr[n].setTime(xsh_.AnmRdr[n]/anm_.anmfps);
-		}
-	}
-};
 
 /*******************************************************************************
 *
@@ -536,14 +381,6 @@ function Mod360(deg) {
 	deg = deg % 360;				 // Compute remainder of any number divided by 360
 return deg;}
 
-//- Make Mesh ------------------------------------------------------------------
-function makMsh() {
-	let geometry = new BoxGeometry(0.01,0.01,0.01); 
-	let material = new MeshBasicNodeMaterial({colorNode:color("black"),transparent:true,opacity:0});
-	let mesh = new Mesh(geometry,material);
-return mesh;}
-
-
 /*******************************************************************************
 *
 *	EXPORTS
@@ -553,8 +390,6 @@ return mesh;}
 export {loadIsland,initIsland,moveIsland,
 		loadFxdObj,initFxdObj,moveFxdObj,
 		loadAnmFlg,moveAnmFlg,
-		loadXACVeh,initXACVeh,
-		loadXSHVeh,initXSHVeh,moveXSHVeh,
 		loadMyPeep,moveMyPeep,
 		loadMyCrew,moveMyCrew,
 	};
@@ -568,5 +403,5 @@ export {loadIsland,initIsland,moveIsland,
 250405:	In development
 250523: Added loadMyCrew/moveMyCrew
 250528: Added animations to loadMyCrew/moveMyCrew
-250529: Marged this Objects Module with Vehicles Module
+250602:	Eliminated makmsh (replaced with Object3D)
 */
