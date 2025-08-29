e93aa1634f2d65957dde404269a2ac1fb0649f20
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-08-29 17:02:38 -0500

    Add files via upload

--- data/fsim_FM2_ocean_gpu.js
+++ data/fsim_FM2_ocean_gpu.js
@@ -877,9 +877,10 @@ let mys_ = {
 let RadTim = 0;					// Start this timer for Linear Segment
 let	rad_ = {
 		// Sound Files (voice plus static)
-		SndSrc: ["https://PhilCrowther.github.io/Aviation/sounds/radio/noise.mp3",
-				 "https://PhilCrowther.github.io/Aviation/sounds/radio/radio.wav"],
-		SndAdr: [0,0],			// Pointer to Radio and Static Sounds
+		SndSrc: ["https://PhilCrowther.github.io/Aviation/sounds/radio/static.mp3",
+				 "https://PhilCrowther.github.io/Aviation/sounds/radio/pilot1.wav"
+				 "https://PhilCrowther.github.io/Aviation/sounds/radio/cvedir.m4a"],
+		SndAdr: [0,0,0],			// Pointer to Radio and Static Sounds
 		// All Segments
 		SegIdx: 0,				// Index to Segment
 		SegSrc: [1,1,1],		// Index to Radio Source
