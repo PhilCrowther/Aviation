56e548bac97c9acf0ca02b1842b4ca64f1c174b3
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-09-01 14:32:02 -0500

    Add files via upload

--- data/fsim_FM2_ocean_gpu.js
+++ data/fsim_FM2_ocean_gpu.js
@@ -169,7 +169,8 @@ let grd_ = {
 		NM2: 0,					// Grid 2 Normal Map
 		// Indices
 		Col: 0,
-		DfS: "https://PhilCrowther.github.io/Aviation/textures/ocean/transition1F_color3.png",
+		DfS: "https://PhilCrowther.github.io/Aviation/textures/ocean/transition1F_color4.png", // texture
+		DfD: "https://PhilCrowther.github.io/Aviation/textures/ocean/ocean_detail_bw.png", // detail
 		Mtl: [0.5,0.5,0.5],		// Metalness (1 for max reflection)
 		RfS: "https://PhilCrowther.github.io/Aviation/textures/ocean/transition5.png",
 		Ruf: [0.5,0.5,0.5],		// Roughness (0 for max reflection)
