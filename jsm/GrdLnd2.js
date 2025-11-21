4bdc80b4a99edb0b91311770a0d70c16eb27bca5
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2025-11-21 12:41:07 -0800

    Add files via upload

--- jsm/GrdLnd2.js
+++ jsm/GrdLnd2.js
@@ -274,7 +274,7 @@ function initGr0Mat(grd_,gen_) {
 		DatTxt.anisotropy = gen_.maxAns;
 		DatTxt.needsUpdate = true;
 //		gt0_.G1MPtr[n] = new MeshLambertNodeMaterial({colorNode: texture(DatTxt)});		
-		gt0_.G0MPtr[n] = new MeshLambertNodeMaterial({colorNode: texture(DatTxt).add(texture(DifTxt)});
+		gt0_.G0MPtr[n] = new MeshLambertNodeMaterial({colorNode: texture(DatTxt).add(texture(DifTxt))});
 		// Gr5Source = Resized Gr4Data
 		// Note: Dividing a Repeated Data Can Lead to Odd Results
 		// e.g. If Repeat X10 and then divide by 10, result = Data
@@ -312,7 +312,7 @@ function initGr1Mat(grd_,gen_) {
 		DatTxt.anisotropy = gen_.maxAns;
 		DatTxt.needsUpdate = true;
 //		gt1_.G1MPtr[n] = new MeshLambertNodeMaterial({colorNode: texture(DatTxt)});
-		gt1_.G0MPtr[n] = new MeshLambertNodeMaterial({colorNode: texture(DatTxt).add(texture(DifTxt)});
+		gt1_.G0MPtr[n] = new MeshLambertNodeMaterial({colorNode: texture(DatTxt).add(texture(DifTxt))});
 		// Gr6Source = Resized Gr5Data		
 		gen_.contxt.putImageData(gt1_.G1DPtr[n],0,0);
 		gen_.contxt.drawImage(gen_.canvas,0,0,dtSize,dtSize,0,0,Gr2Siz,Gr2Siz);
