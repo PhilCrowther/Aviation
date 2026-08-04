561b5bec971fbaf6b0e4bf671c0beb146eb0a5d9
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-08-03 23:02:49 -0500

    Add files via upload

--- data/fsim_SF1_land_gpu.js
+++ data/fsim_SF1_land_gpu.js
@@ -1,7 +1,7 @@
 ﻿
 /********************************************************************************
 *
-*	FSIM SF1 DATA: 260802
+*	FSIM SF1 DATA: 260803
 *
 *********************************************************************************
 
@@ -213,18 +213,19 @@ let mnt_ = {
 		VrtAdj: [-25]			// Vertical Adjustment
 	};
 //- Static Objects -------------//----------------------------------------------
-//- 0 = Hangar;
+//- 0 = Hangars;
 let fxd_ = {
-		ObjNum: 1,
-		ObjSrc: ["https://PhilCrowther.github.io/Aviation/scenery/models/hangar.glb"],
-		ObjTxt: [0],
-		ObjAdr: [0],			// Loaded Object
-		ObjSiz: [Ft2Mtr],		// Scale
-		RndOrd: [0],			// renderOrder
-		ObjRot: [0],			// Rotation
-		MapPos: [0],			// Relative Position
-		ObjRef: [0],			// Parent
-		VrtAdj: [-mnt_.VrtAdj[0]] // Vertical Adjustment
+		ObjNum: 2,
+		ObjSrc: ["https://PhilCrowther.github.io/Aviation/scenery/models/hangar.glb",
+				 "https://PhilCrowther.github.io/Aviation/scenery/models/hangar2.glb"],
+		ObjTxt: [0,0],
+		ObjAdr: [0,0],			// Loaded Object
+		ObjSiz: [Ft2Mtr,Ft2Mtr],	// Scale
+		RndOrd: [0,0],			// renderOrder
+		ObjRot: [0,0],			// Rotation
+		MapPos: [0,0],			// Relative Position
+		ObjRef: [0,0],			// Parent
+		VrtAdj: [-mnt_.VrtAdj[0],-mnt_.VrtAdj[0]] // Vertical Adjustment
 	};
 
 //= MOVING AIRPLANES ===========//==============================================
