dc7b4eec7b2fea63966889eee5ec78288cf68ce4
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-08-04 23:27:14 -0500

    Add files via upload

--- jsm/Effects2.js
+++ jsm/Effects2.js
@@ -504,7 +504,7 @@ function loadAAAGun(aaf_,gen_) {
 			aaf_.FirPtr[n] = new PositionalAudio(gen_.listnr);
 			gen_.audoLd.load(aaf_.FirSrc, function(buffer) {
 				aaf_.FirPtr[n].setBuffer(buffer);
-				init1Sound(aaf_.FirPtr[n],RefDst,aaf_.SndVol,1,0,aaf_.FirMsh[n]);
+				init1Sound(aaf_.FirPtr[n],RefDst,aaf_.SndVol,2,0,aaf_.FirMsh[n]); // 2X speed
 				aaf_.FirPtr[n].add(aaf_.FirMsh[n]);
 			});
 			//	Ending Explosion
@@ -564,7 +564,7 @@ function moveAAAGun(aaf_,air_,gen_,tim_) {
 	for (let n = 0; n < aaf_.ObjNum; n ++) {
 		//- Gunfire ---------------------------------------------------------
 		// Start Delay
-		if (aaf_.FirFlg[n]) { // Compute Delay and Start Countdown
+		if (aaf_.FirFlg[n] && !aaf_.FirPtr[n].isPlaying()) { // Compute Delay and Start Countdown		
 			let X = aaf_.GunPtr[n].position.x; // SndMsh attached to SmkPtr
 			let Z = aaf_.GunPtr[n].position.z;
 			let delay = (Math.sqrt(X*X+Z*Z)/343); // In Seconds
