275743683ed67c123653dc88adcbf4e7bb8a821d
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-08-07 02:40:51 -0500

    Add files via upload

--- data/fsim_SF1_land_gpu.js
+++ data/fsim_SF1_land_gpu.js
@@ -577,7 +577,7 @@ let aaf_ = {
 		SndFlg: [],				// 1 = Start Explosion Sound
 		SndSrc: "https://PhilCrowther.github.io/Aviation/sounds/fx/aaa.mp3",
 		SndPtr: [],
-		SndVol: 15,				// Volume
+		SndVol: 10,				// Volume
 		SndMsh: [],				// (Object3D)
 		SndDTm: [],
 	};
@@ -620,7 +620,7 @@ let bom_ = {
 		SndFlg: [],				// 1 = Sound Ready to be Triggered
 		SndSrc: "https://PhilCrowther.github.io/Aviation/sounds/fx/exp.mp3",
 		SndPtr: [],
-		SndVol: 15,				// Volume
+		SndVol: 10,				// Volume
 		SndMsh: [],				// Object3D (init by initExpBom)
 		SndDTm: [],				// Delay Time
 		SndRTm: [],				// Remaining Time
