55d5f6eb4171b7bd69486304573168c498185001
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-08-08 16:55:48 -0500

    Add files via upload

--- jsm/Effects.js
+++ jsm/Effects.js
@@ -6,7 +6,7 @@
 
 Copyright 2017-26, Phil Crowther <phil@philcrowther.com>
 Licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-Version dated 7 Aug 2026
+Version dated 8 Aug 2026
 
 @fileoverview
 Subroutines to create an air combat simulation
@@ -525,8 +525,18 @@ function initAAAGun(aaf_,txt_,air_,gen_) {
 	if (aaf_.ObjNum) {
 		aaf_.SmkMap = txt_.ObjTxt[aaf_.SmkMap];
 		initAAGuns(aaf_,air_,gen_);
-		// Create Exploding Center
+		// Initial Flash Geo and Mat
+		let FrLGeo = new LineGeometry();
+		FrLGeo.setPositions([0,0,5, 0,0,15]);
+		let FrLMat = new Line2NodeMaterial({color:"crimson",linewidth:2});
 		for (let n = 0; n < aaf_.ObjNum; n ++) {
+			// Create Flash
+			aaf_.FrLPtr[n] = new Line2(FrLGeo,FrLMat);
+			aaf_.FrLPtr[n].rotation.order = "YXZ";
+			aaf_.FrLPtr[n].position.set(0,2.75,0);
+			aaf_.GunPtr[n].add(aaf_.FrLPtr[n]);
+			aaf_.FrLPtr[n].visible = false;			
+			// Create Exploding Center
 			aaf_.ExpPtr[n] = makeSphere("crimson");
 			aaf_.SmkPtr[n].add(aaf_.ExpPtr[n]);
 		}
@@ -772,6 +782,11 @@ function moveAAGuns(aaf_,air_,gen_,tim_) {
 				if (!i) {
 					aaf_.SmkPtr[n].visible = false;
 					aaf_.FirFlg[n] = 1; 	// 1 = Sound Ready
+				// Flash
+				aaf_.FrLPtr[n].rotation.x = -aaf_.AnmLat[n]*DegRad;
+				aaf_.FrLPtr[n].rotation.y = -(aaf_.AnmLon[n]+180)*DegRad;
+				aaf_.FrLPtr[n].visible = true;
+				aaf_.FrLTim[n] = 0.1;
 				}
 			}
 			// Continue Bullets
@@ -805,6 +820,11 @@ function moveAAGuns(aaf_,air_,gen_,tim_) {
 				aaf_.AAAPtr[n][i].position.x = aaf_.AAAMpP[n][i].x - air_.MapPos.x;
 				aaf_.AAAPtr[n][i].position.y = aaf_.AAAMpP[n][i].y - air_.MapPos.y;
 				aaf_.AAAPtr[n][i].position.z = air_.MapPos.z - aaf_.AAAMpP[n][i].z;
+			}
+			// Flash
+			if (aaf_.FrLPtr[n].visible = true) {
+				aaf_.FrLTim[n] = aaf_.FrLTim[n] - tim_.DLTime;
+				if (aaf_.FrLTim[n] < 0) aaf_.FrLPtr[n].visible = false;
 			}
 		} // end of i
 		// Smoke Relative Position
@@ -1586,4 +1606,5 @@ export {
 260801:	Shorten Ending Sequences
 260802: Move Effects Sounds from Objects Module; Elimiate moveEffSnd and play EffSnd subroutines; Replace aag_ with aaf_
 260805: Eliminate xsg_
+260808: Show guns firing (FrL)
 */
