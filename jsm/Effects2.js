be1ab91018db1f9a4b7857d93167b8529cc6168b
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-08-16 23:47:15 -0500

    Add files via upload

--- jsm/Effects2.js
+++ jsm/Effects2.js
@@ -1044,7 +1044,7 @@ function initXSHGun(xsg_,gen_) {
 	let FirMat = new Line2NodeMaterial({color:"crimson",linewidth:2});
 	//- EACH GUN ---------------------------------------------------------------
 	for (let n = 0; n < xsg_.ObjNum; n ++) {
-		xsg_.Parent[n].add(xsg_.GunPtr[n];
+		xsg_.Parent[n].add(xsg_.GunPtr[n]);
 		//.	Create Graphics ....................................................
 		//.	Gun Flash
 		xsg_.FirPtr[n] = new Line2(FirGeo,FirMat);
