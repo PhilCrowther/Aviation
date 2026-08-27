982867218d5cd32f1ee02bebc4b8732ef394bf5d
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-08-27 14:28:24 -0500

    Add files via upload

--- jsm/Effects.js
+++ jsm/Effects.js
@@ -1119,7 +1119,7 @@ function initXSHGun(xsg_,gen_) {
 		xsg_.SmkPtr[n] = new Sprite(xsg_.SmkMat[n]);
 		xsg_.SmkPtr[n].position.set(0,0,10);
 //		xsg_.SmkPtr[n].scale.set(15,15,15);
-		xsg_.SmkPtr[[n].scale.setScalar(15);
+		xsg_.SmkPtr[n].scale.setScalar(15);
 		xsg_.GunPtr[n].add(xsg_.SmkPtr[n]);
 		xsg_.SmkPtr[n].visible = false;
 		//.	Sounds .............................................................
