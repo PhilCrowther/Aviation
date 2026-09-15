320b9b4d7635a7b2c1100048f7b2b22b720db5f0
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-09-15 14:50:55 -0500

    Add files via upload

--- data/fsim_SF1_land_gpu.js
+++ data/fsim_SF1_land_gpu.js
@@ -674,8 +674,6 @@ let bms_ = {
 		GroFlg: [],				// Grow Smoke (after first use)
 		SmkCol: [],				// Smoke Color (vector3)
 		SubSiz: [],				// Smoke Size Subtraction (default = 0.01; test = 0.05)
-		Global: [],				// Global Opacity
-		OpaVal: [],				// Global Opacity Value
 	}
 
 //=	MY SOUNDS ==================//==============================================
