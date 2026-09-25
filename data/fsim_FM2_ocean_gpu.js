e570a0291f6114360c3caade93a8f862c6b20877
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-09-25 01:24:16 -0500

    Add files via upload

--- data/fsim_FM2_ocean_gpu.js
+++ data/fsim_FM2_ocean_gpu.js
@@ -1,7 +1,7 @@
 ﻿
 /*******************************************************************************
 *
-*	FSIM FM2 DATA: 260918
+*	FSIM FM2 DATA: 260925
 *
 ********************************************************************************
 
@@ -430,6 +430,24 @@ let wak_ = {
 //- Ship Smoke (destroyer only) ------------------------------------------------
 let xss_ = {
 		ObjNum: 0,				// Number of Stacks
+		//	Sprite Inputs
+		Speed0: 0,				// Speed - Vector2(0.2,5)
+		LifRng: 0,				// Life - Ranbge - Vector2(.1,1)
+		LifTim: 1,				// Life - Time
+		ColPos: 3,				// Color - Position
+		ColEff: 0.2,			// Color - Effect
+		MatMap: 0,				// Opacity Mask (single material - not used here)
+		MatNod: 0,				// Material Node - Vector2(2.5,1)
+		RotRng: 0,				// Rotate Range - Vector2(.1,4)
+		OpaPwr: 0,				// Opacity Node Computation (used here)
+		OffMin: 0,				// Position - Offset Min - Vector3(-2,3,-2)
+		OffMax: 0,				// Position - Offset Max - Vector3(2,5,2)
+		ScaleR:	0,				// Scale - Range - Vector2(.3,2)
+		ScaleN: 0.3,			// Scale - Node
+		SprCnt: 1000,			// Sprite Count
+		//	Color n
+		Color0: [],				// Color - (vector3)
+		//
 		ObjTxt: [0],			// Shared Texture Reference Number
 		ObjMat: [0],			// Material
 		ObjAdr: [0],			// Emitter Address
