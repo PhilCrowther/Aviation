d4a67e8d69e5265171fab1f9faad685894ad361a
PhilCrowther <97845883+PhilCrowther@users.noreply.github.com>
2026-09-26 13:31:43 -0500

    Add files via upload

--- jsm/Effects.js
+++ jsm/Effects.js
@@ -919,46 +919,59 @@ function moveAAAGun(aaf_,air_,gen_,tim_) {
 
 /*******************************************************************************
 *
-*	AIRPLANE BASED
+*	VOLCANO
 *
 *******************************************************************************/
 
-//= INITIALIZE AIRPLANE SMOKE ==================================================
+//= INIT SMOKE =================//==============================================
 
-function initAirSmk() {
-	for (let n = 0; n < xas_.ObjNum; n ++) {
+function initGrdSmk(grs_) {
+	grs_.ObjTxt = txt_.ObjTxt[SmkBlak];
+	for (let n = 0; n < grs_.ObjNum; n ++) {
+		//- Timer
+		let speed = uniform(.001); // r170
+		let scaledTime = time.add(125).mul(speed); // r170
+		//- Life
 		let lifeRange = range(0.1,1);
-		let offsetRange = range(new Vector3(0,3,0), new Vector3(0,5,0));
-		let speed = uniform(.2);		// r170
-		let scaledTime = time.add(5).mul(speed); // r170
 		let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
-		let scaleRange = range(.01,.02);
-		let rotateRange = range(.1,4);
 		let life = lifeTime.div(lifeRange);
-		let fakeLightEffect = positionLocal.x.oneMinus().max(0.2);
-		let textureNode = texture(xas_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
-		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);
-		//	Color
+		//- Rotation Range
+		let rotateRange = range(.1,4);
+		let textureNode = texture(grs_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
+		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
+		//- Lateral Offset	
+		let offsetRange = range(new Vector3(-.5,3,-.5), new Vector3(1,5,1)); // cone shaped
+		//- Size Range
+		let scaleRange = range(.1,.2);
+		//
+		let fakeLightEffect = positionLocal.y.oneMinus().max(0.2);
+		//-	Wake
 		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
-		//	Material
-		xas_.SmkMat[n] = new SpriteNodeMaterial();
-		xas_.SmkMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
-		xas_.SmkMat[n].opacityNode = opacityNode;
-		xas_.SmkMat[n].positionNode = offsetRange.mul(lifeTime);
-		xas_.SmkMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
-		xas_.SmkMat[n].depthWrite = false;
-		xas_.SmkMat[n].transparent = true;
-		//	Mesh
-		xas_.SmkMsh[n] = new Mesh(new PlaneGeometry(1, 1),xas_.SmkMat[n]);
-		xas_.SmkMsh[n].scale.setScalar(xas_.ObjSiz);
-		xas_.SmkMsh[n].isInstancedMesh = true;
-		xas_.SmkMsh[n].count = 100;
-		xas_.SmkMsh[n].rotation.x = Math.PI/2;
-		xas_.SmkMsh[n].position.z = 10;
+		//-	Material
+		grs_.SmkMat[n] = new SpriteNodeMaterial();
+		grs_.SmkMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
+		grs_.SmkMat[n].opacityNode = opacityNode;
+		grs_.SmkMat[n].positionNode = offsetRange.mul(lifeTime);
+		grs_.SmkMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
+		grs_.SmkMat[n].depthWrite = false;
+		grs_.SmkMat[n].transparent = true;
+		//-	Mesh
+		grs_.SmkMsh[n] = new Mesh(new PlaneGeometry(1, 1),grs_.SmkMat[n]);
+		grs_.SmkMsh[n].scale.setScalar(grs_.ObjSiz);
+		grs_.SmkMsh[n].isInstancedMesh = true;
+		grs_.SmkMsh[n].count = 600; // Increases continuity (was 100)
+		grs_.SmkMsh[n].renderOrder = 1; // This allows the transparent smoke to work with transparent island
 	}
 }
 
+/*******************************************************************************
+*
+*	AIRPLANE SMOKE AND FIRE
+*
+*******************************************************************************/
+
 //= INITIALIZE AIRPLANE FIRE ===================================================
+//	Internal Use Only
 
 function initAirFyr(xaf_) {
 	for (let n = 0; n < xaf_.ObjNum; n ++) {
@@ -1578,98 +1591,6 @@ function moveBomSmk(bms_,n) {
 
 /*******************************************************************************
 *
-*	GROUND BASED SMOKE AND FIRE
-*
-*******************************************************************************/
-
-//= INITIALIZE GROUND SMOKE ====//==============================================
-
-function initGrdSmk(grs_) {
-	grs_.ObjTxt = txt_.ObjTxt[SmkBlak];
-	for (let n = 0; n < grs_.ObjNum; n ++) {
-		//- Timer
-		let speed = uniform(.001); // r170
-		let scaledTime = time.add(125).mul(speed); // r170
-		//- Life
-		let lifeRange = range(0.1,1);
-		let lifeTime = scaledTime.mul(lifeRange).mod(.05); // r170
-		let life = lifeTime.div(lifeRange);
-		//- Rotation Range
-		let rotateRange = range(.1,4);
-		let textureNode = texture(grs_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange))); // r170
-		let opacityNode = textureNode.a.mul(life.oneMinus().pow(50),0.1);	
-		//- Lateral Offset	
-		let offsetRange = range(new Vector3(-.5,3,-.5), new Vector3(1,5,1)); // cone shaped
-		//- Size Range
-		let scaleRange = range(.1,.2);
-		//
-		let fakeLightEffect = positionLocal.y.oneMinus().max(0.2);
-		//-	Wake
-		let smokeColor = mix(color(0xe0e0e0), color(0xd0d0d0), positionLocal.y.mul(3).clamp());
-		//-	Material
-		grs_.SmkMat[n] = new SpriteNodeMaterial();
-		grs_.SmkMat[n].colorNode = mix(color("white"), smokeColor, life.mul(2.5).min(1)).mul(fakeLightEffect);
-		grs_.SmkMat[n].opacityNode = opacityNode;
-		grs_.SmkMat[n].positionNode = offsetRange.mul(lifeTime);
-		grs_.SmkMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
-		grs_.SmkMat[n].depthWrite = false;
-		grs_.SmkMat[n].transparent = true;
-		//-	Mesh
-		grs_.SmkMsh[n] = new Mesh(new PlaneGeometry(1, 1),grs_.SmkMat[n]);
-		grs_.SmkMsh[n].scale.setScalar(grs_.ObjSiz);
-		grs_.SmkMsh[n].isInstancedMesh = true;
-		grs_.SmkMsh[n].count = 600; // Increases continuity (was 100)
-		grs_.SmkMsh[n].renderOrder = 1; // This allows the transparent smoke to work with transparent island
-	}
-}
-
-//= INITIALIZE GROUND FIRE =====//===============================================
-
-function initGrdFyr(grf_) {
-	for (let n = 0; n < grf_.ObjNum; n ++) {
-		// create nodes
-		let lifeRange = range(.1,1);
-		let speed = uniform(.01);
-		let scaledTime = time.add(5).mul(speed);
-		let lifeTime = scaledTime.mul(lifeRange).mod(1);
-		let scaleRange = range(.3,2);
-		let rotateRange = range(.1,4);
-		let life = lifeTime.div(lifeRange);
-		let fakeLightEffect = positionLocal.y.oneMinus().max(0.2);
-		let textureNode = texture(grf_.ObjTxt, rotateUV(uv(),scaledTime.mul(rotateRange)));
-		let opacityNode = textureNode.a.mul(life.oneMinus());
-		let smokeColor = mix(color(0x2c1501),color(0x222222),positionLocal.y.mul(3).clamp());
-		//-	Smoke
-		grf_.SmkMat[n] = new SpriteNodeMaterial();
-		grf_.SmkMat[n].colorNode = mix(color(0xf27d0c),smokeColor,life.mul(2.5).min(1)).mul(fakeLightEffect);
-		grf_.SmkMat[n].opacityNode = opacityNode;
-		grf_.SmkMat[n].positionNode = range(new Vector3(-2,3,-2), new Vector3(2,5,2)).mul(lifeTime);
-		grf_.SmkMat[n].scaleNode = scaleRange.mul(lifeTime.max(0.3));
-		grf_.SmkMat[n].depthWrite = false;
-		//
-		grf_.SmkMsh[n] = new Mesh(new PlaneGeometry(1,1),grf_.SmkMat[n]);
-		grf_.SmkMsh[n].scale.setScalar(grf_.ObjSiz);
-		grf_.SmkMsh[n].count = 2000;
-		grf_.SmkMsh[n].renderOrder = 1;
-		//- Fire
-		grf_.FyrMat[n] = new SpriteNodeMaterial();
-		grf_.FyrMat[n].colorNode = mix(color(0xb72f17),color(0xb72f17),life);
-		grf_.FyrMat[n].positionNode = range(new Vector3(-1,1,-1),new Vector3(1,2,1)).mul(lifeTime);
-		grf_.FyrMat[n].scaleNode = grf_.SmkMat[n].scaleNode;
-		grf_.FyrMat[n].opacityNode = opacityNode.mul(.5);
-		grf_.FyrMat[n].blending = AdditiveBlending;
-		grf_.FyrMat[n].transparent = true;
-		grf_.FyrMat[n].depthWrite = false;
-		//
-		grf_.FyrMsh[n] = new Mesh(new PlaneGeometry(1,1),grf_.FyrMat[n]);
-		grf_.FyrMsh[n].scale.setScalar(grf_.ObjSiz);
-		grf_.FyrMsh[n].count = 1000;
-		grf_.FyrMsh[n].renderOrder = 1;
-	}
-}
-
-/*******************************************************************************
-*
 *	EFFECTS SOUNDS
 *
 *******************************************************************************/
@@ -1743,7 +1664,7 @@ export {
 	initXSHGun,moveXSHGun,				// Ship Guns
 	initSmkTrl,moveSmkTrl,				// Sprite Smoke Trail
 	initExpBom,moveExpBom,				// Bombs
-	initGrdSmk,initGrdFyr,				// Ground Smoke and Fire
+	initGrdSmk,							// Ground Smoke and Fire
 	stopEffSnd,							// Sounds
 };
 
